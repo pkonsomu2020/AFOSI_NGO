89c32ea0490dd41ebfe8b8e8cad24316a5f53fac
pkonsomu2020 <pkonsomu2021@gmail.com>
2026-04-03 23:34:17 +0300

    fix: make View All Projects button text visible on dark background

--- src/components/ProgramsSection.tsx
+++ src/components/ProgramsSection.tsx
@@ -190,7 +190,7 @@ const ProgramsSection = () => {
               <Button size="lg" className="bg-orange-500 hover:bg-orange-600 text-white rounded-full px-8 shadow-xl shadow-orange-500/30">
                 Become a Partner
               </Button>
-              <Button size="lg" variant="outline" className="border-white/20 text-white hover:bg-white/10 rounded-full px-8" asChild>
+              <Button size="lg" variant="outline" className="border-white/20 text-gray-900 bg-white hover:bg-gray-100 rounded-full px-8" asChild>
                 <a href="/projects">View All Projects</a>
               </Button>
             </div>
